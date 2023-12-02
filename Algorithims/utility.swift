3fa8ea43aae9f83531bdca31f5e31ec5a88d2815
William Eze <eze.w@etoncollege.org.uk>
2023-12-02 14:29:44 +0000

    removed redundant comments

--- Algorithims/utility.swift
+++ Algorithims/utility.swift
@@ -8,34 +8,6 @@
 import Foundation
 
 class Algorithims{
-//    func quickSort(data:[Int]) -> [Int]{
-//        let pivot = data[0]
-//        var left = [Int]()
-//        var right = [Int]()
-//        
-//        for (i,v) in data.enumerated(){
-//            if i != 0 {
-//                if v <= pivot{
-//                    left.append(v)
-//                }
-//                else {
-//                    right.append(v)
-//                }
-//            }
-//        }
-//        var final = [Int]()
-//        if left.count > 0 {
-//            final.append(contentsOf: quickSort(data: left))
-//        }
-//        final.append(pivot)
-//        
-//        if right.count > 0 {
-//            final.append(contentsOf: quickSort(data: right))
-//        }
-//        
-//        return final
-//    }
-    
     func linearSearch(data: [Int], target: Int) -> Bool {
         if data.count < 1{
             return false
